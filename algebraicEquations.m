dbf97b855f18fc113f1dbbe5cde8a2098a729bb8
Krishna <krishnakumar@imperial.ac.uk>
2017-09-21 18:06:56 +0200

    created output function

--- algebraicEquations.m
+++ algebraicEquations.m
@@ -1,8 +1,8 @@
-function [algebraiceqn_residual] = algebraicEquations(z,x,param)
-    algebraiceqn_residual1 = param.Q_plus - x(6) + 10^(-z(1)) - z(2) - z(3) - z(4);
-    algebraiceqn_residual2 = z(2) - param.K2*x(1)/(param.K2 + 10^(-z(1)));
-    algebraiceqn_residual3 = z(3) - param.K3*x(3)/(param.K3 + 10^(-z(1)));
-    algebraiceqn_residual4 = z(4) - param.K1*x(5)/(param.K1 + 10^(-z(1)));
-    
+function [algebraiceqn_residual] = algebraicEquations(Z,X,param)
+    algebraiceqn_residual1 = param.Q_plus - X(6) + 10^(-Z(1)) - Z(2) - Z(3) - Z(4);
+    algebraiceqn_residual2 = Z(2) - param.K2*X(1)/(param.K2 + 10^(-Z(1)));
+    algebraiceqn_residual3 = Z(3) - param.K3*X(3)/(param.K3 + 10^(-Z(1)));
+    algebraiceqn_residual4 = Z(4) - param.K1*X(5)/(param.K1 + 10^(-Z(1)));
+
     algebraiceqn_residual = [algebraiceqn_residual1;algebraiceqn_residual2;algebraiceqn_residual3;algebraiceqn_residual4];
 end
---
+++ outputFunction.m
@@ -0,0 +1,9 @@
+function h = outputFunction(X,param)
+
+    % output1 = X(1);
+    % output2 = X(2);
+    % output3 = X(3);
+    % output4 = X(4);
+    %
+    h = [X(1);X(2);X(3);X(4)]; % in this specific example, the 1st four states were chosen as the outputs
+end
